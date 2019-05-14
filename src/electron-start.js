948b3e64a2fb84bc2b0fdd31470f8af917d0cf7f
Emmanuel Robert Ssebaggala <emmanuel.ssebaggala@bodastage.com>
2019-05-14 19:22:56 +0300

    Hide window that runs parsers

--- src/electron-start.js
+++ src/electron-start.js
@@ -11,8 +11,8 @@ let mainWindow
 
 //Create parse cm background renderer
 function createParseCMBgWindow() {
-  //result = new BrowserWindow({"show": false, webPreferences: {nodeIntegration: true}})
-  result = new BrowserWindow({"show": true,width: 900, height: 600, webPreferences: {nodeIntegration: true}})
+  result = new BrowserWindow({"show": false, webPreferences: {nodeIntegration: true}})
+  //result = new BrowserWindow({"show": true,width: 900, height: 600, webPreferences: {nodeIntegration: true}})
   result.loadURL('file://' + __dirname + '/../background/parse_cm_dumps.html')
   result.on('closed', () => {
     console.log('background window closed')
