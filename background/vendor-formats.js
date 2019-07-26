77af75449a8e456b44295439700d597cb958919a
Emmanuel Robert Ssebaggala <emmanuel.ssebaggala@bodastage.com>
2019-07-26 03:35:00 +0300

    Update ztexlsparser to 1.2.4
    This version get's the export date from the file name

---
+++ background/vendor-formats.js
@@ -0,0 +1,47 @@
+const VENDOR_CM_FORMATS = {
+	'ERICSSON': ['BULKCM','CNAIV2'],
+	'HUAWEI': ['GEXPORT_XML','NBI_XML','CFGMML'],
+	'ZTE': ['BULKCM','XLS'],
+	'NOKIA': ['RAML'],
+	'BODASTAGE': ['BCF']
+}
+
+const VENDOR_PM_FORMATS = {
+	'ERICSSON': ['BULKCM','CNAIV2'],
+	'HUAWEI': ['GEXPORT_XML','NBI_XML','CFGMML'],
+	'ZTE': ['BULKCM','XLS'],
+	'NOKIA': ['RAML'],
+	'BODASTAGE': []
+}
+
+const VENDOR_FM_FORMATS = {
+	'ERICSSON': ['BULKCM','CNAIV2'],
+	'HUAWEI': ['GEXPORT_XML','NBI_XML','CFGMML'],
+	'ZTE': ['BULKCM','XLS'],
+	'NOKIA': ['RAML'],
+	'BODASTAGE': []
+}
+
+
+/*Parser for each vendor CM file format*/
+const VENDOR_CM_PARSERS = {
+	'ERICSSON': {
+		'BULKCM': 'boda-bulkcmparser.jar',
+		'CNAIV2': 'boda-ericssoncnaiparser.jar',
+	},
+	'HUAWEI': {
+		'GEXPORT_XML': 'boda-huaweicmobjectparser.jar',
+		'CFGMML': 'boda-huaweimmlparser.jar',
+		'NBI_XML': 'boda-huaweicmxmlparser.jar'
+	},
+	'ZTE': {
+		'BULKCM': 'boda-bulkcmparser.jar',
+		'XLS': 'boda-ztexlscmparser.jar',
+	},
+	'NOKIA': {
+		'RAML': 'boda-nokiacmdataparser.jar'
+	}
+}
+
+
+exports.VENDOR_CM_PARSERS = VENDOR_CM_PARSERS;
