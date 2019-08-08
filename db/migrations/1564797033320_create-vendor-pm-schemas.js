70b5635f7199aed16e5735d8b9007fce4b347cf5
Emmanuel Robert Ssebaggala <emmanuel.ssebaggala@bodastage.com>
2019-08-08 07:13:23 +0300

    Update pm.ericsson migrations

--- db/migrations/1564797033320_create-vendor-pm-schemas.js
+++ db/migrations/1564797033320_create-vendor-pm-schemas.js
@@ -7,12 +7,30 @@ exports.up = (pgm) => {
 	
 	//Ericsson PM
 	pgm.createTable(
-		{schema: "pm", name: "huawei"}, 
+		{schema: "pm", name: "ericsson"}, 
 		{
 			id: "id",
-			counter_id: {type: "integer"},
-			measurement_id: {type: "integer"},
-			counter_value: {type: "integer"},
+			file_name: {type: "varchar(200)", notNull: true},
+			file_format_version: {type: "varchar(100)"},
+			vendor_name: {type: "varchar(100)"},
+			file_header_dnprefix: {type: "varchar(100)"},
+			file_sender_localdn: {type: "varchar(100)"},
+			element_type: {type: "varchar(100)"},
+			collection_begin_time: {type: "timestamp"},
+			collection_end_time: {type: "timestamp"},
+			managed_element_localdn: {type: "varchar(200)"},
+			ne_software_version: {type: "varchar(100)"},
+			meas_infoid: {type: "varchar(100)"},
+			meas_timestamp: {type: "timestamp"},
+			jobid: {type: "integer"},
+			gran_period_duration: {type: "varchar(50)"},
+			gran_period_endtime: {type: "timestamp", default: "NOW()"},
+			reporting_period: {type: "varchar(50)"},
+			managed_element_userlabel: {type: "varchar(100)"},
+			meas_objldn: {type: "varchar(250)"},
+			meas_type: {type: "varchar(100)"},
+			meas_result: {type: "varchar(100)"},
+			suspect: {type: "varchar(10)"},
 			created_at: "createdAt", 
 			modified_at: "createdAt", 
 			created_by: "createdBy",
@@ -22,7 +40,7 @@ exports.up = (pgm) => {
 };
 
 exports.down = (pgm) => {
-	pgm.dropTable({schema: "pm", name: 'huawei'});
+	pgm.dropTable({schema: "pm", name: 'ericsson'}, {ifExists : true});
 	
 	pgm.dropSchema("pm", {ifExists : true} );
 };
