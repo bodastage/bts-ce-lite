9f180a6bd4dab7ff14844c9c2c654a6b26741076
Emmanuel Robert Ssebaggala <emmanuel.ssebaggala@bodastage.com>
2019-08-16 00:11:36 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -35,8 +35,17 @@ extensions = [
 	'sphinx_markdown_tables'
 ]
 
+# for Sphinx-1.3
+from recommonmark.parser import CommonMarkParser
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+
+source_suffix = ['.rst', '.md']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
