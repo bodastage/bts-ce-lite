459bc665ff37c758d81b6ba6da88dc739540c351
Emmanuel Robert Ssebaggala <emmanuel.ssebaggala@bodastage.com>
2019-08-16 00:14:43 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -41,12 +41,6 @@ from recommonmark.parser import CommonMarkParser
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-source_parsers = {
-    '.md': CommonMarkParser,
-}
-
-source_suffix = ['.rst', '.md']
-
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
