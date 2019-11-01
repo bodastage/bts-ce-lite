075516445953b7ec30cd07124021f3ea50d25628
Emmanuel Robert Ssebaggala <emmanuel.ssebaggala@bodastage.com>
2019-11-01 15:40:04 +0300

    Fix Java check typo

--- src/modules/dashboard/dashboard-actions.js
+++ src/modules/dashboard/dashboard-actions.js
@@ -23,7 +23,7 @@ export function addDashboardNotice(notice){
 */
 export function checkIfJavaIsInstalled(){
     return (dispatch, getState) => {
-		var spawn = window.require('child_process').spawn('javaD', ['-version']);
+		var spawn = window.require('child_process').spawn('java', ['-version']);
 		spawn.on('error', function(err){
 			//return callback(err, null);
 			log.error(err);
@@ -35,17 +35,17 @@ export function checkIfJavaIsInstalled(){
 		})
 		
 		spawn.stderr.on('data', function(data) {
-			console.log("spawn.stderr.on('data', function(data) {");
 			data = data.toString().split('\n')[0];
 			var javaVersion = new RegExp('java version').test(data) ? data.split(' ')[2].replace(/"/g, '') : false;
 			if (javaVersion != false) {
 				// TODO: We have Java installed
 				//return callback(null, javaVersion);
+				dispatch(addDashboardNotice(null));
 				
 			} else {
-				log.error("Java cannot be detected on your system. It is required by the application. Download Java from https://www.java.com/en/download",);
+				log.error("Java cannot be detected on your system. It is required by the application. Download Java from https://www.java.com/en/download");
 				dispatch(addDashboardNotice({
-					message: "Java is not installed or not in the system path!",
+					message: "Java cannot be detected on your system. It is required by the application. Download Java from https://www.java.com/en/download",
 					type: 'danger'
 				}))
 				// TODO: No Java installed
