0b97c75874c69d7b9414dbe7c2abd6004c57f6be
Emmanuel Robert Ssebaggala <emmanuel.ssebaggala@bodastage.com>
2019-11-04 17:42:47 +0300

    Fix java installation check

--- src/modules/dashboard/dashboard-actions.js
+++ src/modules/dashboard/dashboard-actions.js
@@ -27,19 +27,23 @@ export function checkIfJavaIsInstalled(){
 		spawn.on('error', function(err){
 			//return callback(err, null);
 			log.error(err);
-			log.error("Java cannot be detected on your system. It is required by the application. Download Java from https://www.java.com/en/download",);
-			dispatch(addDashboardNotice({
-				message: "Java cannot be detected on your system. It is required by the application. Download Java from https://www.java.com/en/download",
-				type: 'danger'
-			}))
 		})
 		
+		spawn.stdout.on('data', function(data) {
+			console.log("spawn.stdout.on:", data);
+		});
+		
 		spawn.stderr.on('data', function(data) {
+			
+			//Check if java is in string data
+			var javaCheck = new RegExp('java', 'i').test(data);
+			
+			//
 			data = data.toString().split('\n')[0];
 			var javaVersion = new RegExp('java version').test(data) ? data.split(' ')[2].replace(/"/g, '') : false;
-			if (javaVersion != false) {
+
+			if (javaCheck != false) {
 				// TODO: We have Java installed
-				//return callback(null, javaVersion);
 				dispatch(clearNotice());
 				
 			} else {
@@ -48,8 +52,7 @@ export function checkIfJavaIsInstalled(){
 					message: "Java cannot be detected on your system. It is required by the application. Download Java from https://www.java.com/en/download",
 					type: 'danger'
 				}))
-				// TODO: No Java installed
-
+				//@TODO: No Java installed
 			}
 		});
         
