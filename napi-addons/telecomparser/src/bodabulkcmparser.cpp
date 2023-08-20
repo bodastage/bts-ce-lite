69276334dfd653a3776782e95a565a7291368f61
Emmanuel Robert Ssebaggala <emmanuel.ssebaggala@bodastage.com>
2023-08-21 00:26:56 +0300

    add licence notice

--- napi-addons/telecomparser/src/bodabulkcmparser.cpp
+++ napi-addons/telecomparser/src/bodabulkcmparser.cpp
@@ -1,3 +1,19 @@
+/**
+
+Copyright 2023 Bodastage Solutions Ltd
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 #include "bodabulkcmparser.h"
 #include <iostream>
 #include "fast-cpp-csv-parser/csv.h"
@@ -216,7 +232,8 @@ string bodastage::BodaBulkCmParser::to_csv_format(string s) {
     string csv_value = s;
 
     if (s.empty()) {
-        csv_value = "\"\"";
+        // csv_value = "\"\"";
+        csv_value = "";
         return csv_value;
     }
 
