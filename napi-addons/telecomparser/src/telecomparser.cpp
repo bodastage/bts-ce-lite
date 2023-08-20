b8ae887eb9f3438e4f12de80de1be8c351f31d35
Emmanuel Robert Ssebaggala <emmanuel.ssebaggala@bodastage.com>
2023-08-21 00:42:14 +0300

    rename parser to boda-telecomparser

--- napi-addons/telecomparser/src/telecomparser.cpp
+++ napi-addons/telecomparser/src/telecomparser.cpp
@@ -1,22 +1,3 @@
-/**
-
-Copyright 2023 Bodastage Solutions Ltd
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-
-@file telecomparser.cpp
-@brief TelecomParser Napi module
-*/
 #include "telecomparser.h"
 #include "bodabulkcmparser.h"
 #include "spdlog/spdlog.h"
@@ -29,7 +10,7 @@ using namespace std;
 
 int bodastage::parse_bulkcm(string input_file, string output_dir, string multivalue_sep, string meta_fields, string extract_parametes, bool separate_vsdata)
 {
-    spdlog::info("boda-bulkcmparser 1.0.0 - {}", input_file);
+    spdlog::info("boda-telecomparser 1.0.0 - {}", input_file);
 
     bodastage::BodaBulkCmParser parser;
 
